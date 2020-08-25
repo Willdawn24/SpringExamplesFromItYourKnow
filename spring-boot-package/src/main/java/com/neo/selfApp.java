b34c61cceeaf37243e5127bee8bd2699538de967
Will <fengjiaju24@gmail.com>
2020-08-26 01:54:30 +0800

    Small modification on main class

---
+++ spring-boot-package/src/main/java/com/neo/selfApp.java
@@ -0,0 +1,9 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+
+public class selfApp {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
